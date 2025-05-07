c94363410dcfc5b4314ad1eb44f141e7efeb76d0
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-05-07 23:04:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1304,9 +1304,8 @@ def Geenrate_MIS_Report():
             st.write("No Transactions")
 
 
-    FD_clients['Issue Date'] = pd.to_datetime(FD_clients['Issue Date'], dayfirst=True, errors='coerce')
+    FD_clients['Issue Date'] = pd.to_datetime(FD_clients['Issue Date'],format='%d-%m-%Y')
     FD_clients['Month-Year'] = FD_clients['Issue Date'].dt.strftime('%B-%Y')
-    FD_clients['Issue Date'] = FD_clients['Issue Date'].dt.strftime('%d-%m-%Y')
     st.write("FD_clients")
     st.dataframe(FD_clients)
     filtered_FD = FD_clients[FD_clients['Month-Year'] == selected_month]
