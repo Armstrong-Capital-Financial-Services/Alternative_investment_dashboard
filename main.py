5a6abeaca2b1bb4c0b13b8284495fe977a203a9b
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-06-23 23:35:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1669,7 +1669,7 @@ def Geenrate_MIS_Report():
                output_filename = f"Investment_Report_{rm_name.replace(' ', '_')}_{selected_quarter_fy.replace(' ', '_')}.pdf"
                temp_path = os.path.join(tempfile.gettempdir(), output_filename)
                pdf_path = create_simple_investment_report(investment_df,rm_name,filtered_smallcase_df,
-               filtered_vested_df,filtered_pms_df,filtered_bonds_df,filtered_fd_df,filtered_aif_df,filtered_bank_df,filtered_insurance_df,filtered_liquiloans_df, filtered_F_real_estate_df,output_path=temp_path,selected_quarter=selected_quarter_fy)
+               filtered_vested_df,filtered_pms_df,filtered_bonds_df,filtered_fd_df,filtered_aif_df,filtered_bank_df,filtered_insurance_df,filtered_liquiloans_df, filtered_F_real_estate_df,output_path=temp_path,selected_cy= selected_calender_year,investment_df_cy=investment_df_cy)
 
           if pdf_path and os.path.exists(pdf_path):
                 with open(pdf_path, "rb") as f:
