fcddbab836e592182eb026fc1a4516ca8a1ab7a2
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-06-24 00:01:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1661,7 +1661,7 @@ def Geenrate_MIS_Report():
       with st.spinner("Generating..."):
           output_filename = f"Investment_Report_{rm_name.replace(' ', '_')}_{selected_month.replace(' ', '_')}.pdf"
           temp_path = os.path.join(tempfile.gettempdir(), output_filename)
-          pdf_path = create_simple_investment_report(selected_month,rm_name,filtered_smallcase_df,
+          pdf_path = create_simple_investment_report(selected_month,investment_df,rm_name,filtered_smallcase_df,
                filtered_vested_df,filtered_pms_df,filtered_bonds_df,filtered_fd_df,filtered_aif_df,filtered_bank_df,filtered_insurance_df,filtered_liquiloans_df, filtered_F_real_estate_df,output_path=temp_path)
               
           if pdf_path and os.path.exists(pdf_path):
