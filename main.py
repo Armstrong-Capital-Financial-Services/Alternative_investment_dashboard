71b0e09d63452a005d7ae60621a83338869ae891
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-11-03 00:40:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1964,7 +1964,7 @@ def Geenrate_MIS_Report():
           if timeperiod == 'Monthly':
               output_filename = f"Investment_Report_{rm_name.replace(' ', '_')}.pdf"
               temp_path = os.path.join(tempfile.gettempdir(), output_filename)
-              pdf_path = create_simple_investment_report(investment_df,rm_name,filtered_smallcase_df,
+              pdf_path = create_simple_investment_report(selected_month,investment_df,rm_name,filtered_smallcase_df,
               filtered_vested_df,filtered_pms_df,filtered_bonds_df,filtered_fd_df,filtered_aif_df,filtered_bank_df,filtered_insurance_df, filtered_FracRealEstate_df,temp_path)
               if pdf_path and os.path.exists(pdf_path):
                 with open(pdf_path, "rb") as f:
@@ -1981,7 +1981,7 @@ def Geenrate_MIS_Report():
           elif timeperiod == 'Calender Year':
               output_filename = f"Investment_Report_{rm_name.replace(' ', '_')}.pdf"
               temp_path = os.path.join(tempfile.gettempdir(), output_filename)
-              pdf_path = create_simple_investment_report(investment_df_cy,rm_name,filtered_smallcase_df,
+              pdf_path = create_simple_investment_report(selected_calender_year,investment_df_cy,rm_name,filtered_smallcase_df,
               filtered_vested_df,filtered_pms_df,filtered_bonds_df,filtered_fd_df,filtered_aif_df,filtered_bank_df,filtered_insurance_df, filtered_FracRealEstate_df,temp_path)
               if pdf_path and os.path.exists(pdf_path):
                 with open(pdf_path, "rb") as f:
