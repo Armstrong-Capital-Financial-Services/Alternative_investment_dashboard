83d18429b93ddcce805f3507761abe6ab704aae0
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-11-02 21:29:29 +0530

    Update AIF.py

--- pages/AIF.py
+++ pages/AIF.py
@@ -1,3 +1,43 @@
+from enum import unique
+import psycopg2
+import pandas as pd
+import plotly.express as px
+import streamlit
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from rich.jupyter import display
+from streamlit import table
+from datetime import date
+import datetime
+from psycopg2 import sql
+from supabase import create_client
+from pdf_generator_fixed import create_simple_investment_report
+st.set_page_config(layout="wide")
+
+db_config = {
+    "user": st.secrets["DB"]["USER"],
+    "password": st.secrets["DB"]["PASSWORD"],
+    "host": st.secrets["DB"]["HOST"],
+    "port": st.secrets["DB"]["PORT"],
+    "dbname": st.secrets["DB"]["NAME"]
+}
+
+def fetch_table_data(connection, table_name):
+    """Fetch data from a PostgreSQL table and return as a Pandas DataFrame."""
+    try:
+        query = f'SELECT * FROM "{table_name}";'
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            columns = [desc[0] for desc in cursor.description]
+            rows = cursor.fetchall()
+            return pd.DataFrame(rows, columns=columns)
+    except psycopg2.Error as e:
+        print(f"Error fetching data from {table_name}: {e}")
+        return pd.DataFrame()
+
+
 def AIF_Analysis(display=True):
   with psycopg2.connect(**db_config) as connection:
        aif_df=fetch_table_data(connection=connection,table_name="AIF")
@@ -8,3 +48,5 @@ def AIF_Analysis(display=True):
 
     with col1:
       st.metric("Total AUM",aif_df['Current Value'].sum(),border=True)
+
+AIF_Analysis(display=True)
