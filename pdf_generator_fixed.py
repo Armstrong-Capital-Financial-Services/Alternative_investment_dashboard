3ebd5f4f05164461088dc1111daaa0e2c1dba5d7
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-11-02 23:33:00 +0530

    Update pdf_generator_fixed.py

--- pdf_generator_fixed.py
+++ pdf_generator_fixed.py
@@ -122,7 +122,7 @@ def create_simple_investment_report(investment_df,
         add_table("PMS", pms_clients, ['NAME', 'INVESTED AMOUNT', 'STRATEGY'])
         add_table("FD", fd_clients, ['NAME', 'ISSUE DATE', 'INVESTED AMOUNT', 'CHANNEL PARTNER'])
         add_table("Bonds", bonds_clients, ['NAME', 'INVESTED AMOUNT', 'PAN', 'ISSUE NAME', 'TYPE'])
-        add_table("AIF",aif_clients, ['NAME','INVESTED AMOUNT','CURRENT VALUE'])
+        #add_table("AIF",aif_clients, ['NAME','INVESTED AMOUNT','CURRENT VALUE'])
         add_table("Banking Products",bank_clients, ['NAME','INVESTED AMOUNT','CURRENT VALUE'])
         add_table("Insurance",insurance_clients, ['NAME','INVESTED AMOUNT','CURRENT VALUE'])
         #add_table("Liquiloans",liqiloans_clients, ['NAME','INVESTED AMOUNT','CURRENT VALUE'])
