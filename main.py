87afec6c3e0e003334dd748360a637ecd4805446
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-11-02 21:22:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1979,16 +1979,16 @@ def Geenrate_MIS_Report():
    #       else:
    #             st.error("Failed to generate PDF report.")
 
-def AIF_Analysis(display=True):
-  with psycopg2.connect(**db_config) as connection:
-       aif_df=fetch_table_data(connection=connection,table_name="AIF")
-  if display:
-    col0,col1=st.columns(2)
-    with col0:
-      st.metric("Total Active Clients",len(aif_df),border=True)
+#def AIF_Analysis(display=True):
+#  with psycopg2.connect(**db_config) as connection:
+#       aif_df=fetch_table_data(connection=connection,table_name="AIF")
+#  if display:
+#    col0,col1=st.columns(2)
+#    with col0:
+#      st.metric("Total Active Clients",len(aif_df),border=True)
 
-    with col1:
-      st.metric("Total AUM",aif_df['Current Value'].sum(),border=True)
+#    with col1:
+#      st.metric("Total AUM",aif_df['Current Value'].sum(),border=True)
 
 
 
